d8ebcbdec885e0e3874c47546d02fa368b425284
Felipe Jesus <110308764+felipeollveira@users.noreply.github.com>
2025-01-09 17:11:28 -0300

    :feat filter in real time

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
+import '../App.css';
+import SkeletonCard from './SkeletonCard';
 
 const PhotoCard = ({ photo }) => {
+  const imageStyle = {
+    backgroundColor: "#cccccc", 
+    height: "200px", 
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    color: "#666666", 
+  };
+
   return (
     <div className="col">
       <div className="card shadow-sm photo-card">
-        <img src={photo.url} alt={photo.title} className="card-img-top" />
+        {photo.url ? (
+          <img src={photo.url} alt={photo.title} className="card-img-top skeleton" />
+        ) : (
+          <div style={imageStyle}>Imagem indisponível</div>
+        )}
         <div className="card-body">
-     
           <h5 className="card-title">{photo.title}</h5>
-     
         </div>
       </div>
     </div>
   );
 };
+
 
 export default PhotoCard;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,34 @@
-
+    import { useEffect } from "react";
 
   const SearchBar = ({ searchTerm, setSearchTerm, handleSearch, handleKeyDown }) => {
-    return (
-      <div className="d-flex w-100">
-        <input
-          type="text"
-          className="form-control"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          onKeyDown={handleKeyDown} //enter
-          placeholder="Digite uma palavra-chave"
-        />
-        <button onClick={handleSearch} className="btn btn-primary ms-2">
-        🔍
-        </button>
-      </div>
-    );
-  };
+
+
+      useEffect(() => {
+        const atraso = setTimeout(() => { // Estrategia pra nao sobrecarregar e nao pesquisar a cada palavra digitada
+          // 
+          handleSearch(searchTerm);
+        }, 900); 
+    
+        return () => clearTimeout(atraso); // Limpa o timeout anterior
+      }, [searchTerm, handleSearch]); 
+    
+      return (
+        <div className="d-flex w-100">
+          <input
+            type="text"
+            className="form-control"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Digite uma palavra-chave"
+          />
+          <button onClick={() => handleSearch(searchTerm)} className="btn btn-primary ms-2">
+            🔍
+          </button>
+        </div>
+      );
+    };
+    
+ export default SearchBar;
   
-  export default SearchBar;
+
   
